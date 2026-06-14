b8cf5c1783c4722167dfab0e85ffa755ded49c91
hasnatfahmidkhan <hasnatfahmidkhan@gmail.com>
2026-06-14 17:24:29 +0600

    added 6th and 7th query solution

--- solution.sql
+++ solution.sql
@@ -236,3 +236,38 @@ SELECT
 FROM
   users AS u
   LEFT JOIN bookings AS b ON u.user_id = b.user_id;
+
+
+-- =========================================================================
+-- Query 6: Find all ticket bookings where the total cost is strictly higher than the average cost of all ticket bookings.
+-- =========================================================================
+SELECT
+  booking_id,
+  match_id,
+  total_cost
+FROM
+  bookings
+WHERE
+  total_cost > (
+    SELECT
+      Round(avg(total_cost))
+    FROM
+      bookings
+  );
+
+
+-- =========================================================================
+-- Query 7: Retrieve the top 2 most expensive matches sorted by base ticket price, skipping the absolute highest premium match.
+-- =========================================================================
+SELECT
+  match_id,
+  fixture,
+  base_ticket_price
+FROM
+  matches
+ORDER BY
+  base_ticket_price DESC
+LIMIT
+  2
+OFFSET
+  1;
